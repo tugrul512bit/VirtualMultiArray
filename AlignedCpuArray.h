69ab92cf590e703c1f699833f72271aa4d45a21e
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-02-02 18:23:50 +0300

    Removed mlock/munlock, inserted opencl equivalent

--- AlignedCpuArray.h
+++ AlignedCpuArray.h
@@ -10,19 +10,41 @@
 
 #include<iostream>
 #include<sys/mman.h>
+#include"ClCommandQueue.h"
+#include"ClContext.h"
 #include"CL/cl.h"
 
 template<typename T>
 class AlignedCpuArray
 {
 public:
-	AlignedCpuArray():size(0),pinned(false),arr(nullptr){}
-	AlignedCpuArray(size_t sizeP, int alignment=4096, bool pinArray=false):size(sizeP),pinned(pinArray),arr((T *)aligned_alloc(alignment,sizeof(T)*sizeP))
+	AlignedCpuArray():size(0){ pinned=false; arr=nullptr;}
+	AlignedCpuArray(std::shared_ptr<ClContext> ctx, std::shared_ptr<ClCommandQueue> q,size_t sizeP, int alignment=4096, bool pinArray=false):size(sizeP)
 	{
+		cq=q;
+		cntxt=ctx;
+		pinned = pinArray;
 		// todo: optimize for pinned array
 		if(pinned)
 		{
+			cl_int err;
+			mem = clCreateBuffer(*(cntxt->ctxPtr()),CL_MEM_ALLOC_HOST_PTR,size,nullptr,&err);
+			if(CL_SUCCESS!=err)
+			{
+				std::cout<<"error: host buffer"<<std::endl;
+			}
+			arr = (T *) clEnqueueMapBuffer(cq->getQueue(),mem,CL_TRUE,CL_MAP_READ | CL_MAP_WRITE,0,size,0,nullptr,nullptr,&err);
+			if(CL_SUCCESS!=err)
+			{
+				std::cout<<"error: clmap"<<std::endl;
+			}
+
 			if(ENOMEM==mlock(arr,size)){ std::cout<<"Not enough pinned memory."<<std::endl; pinned = false; };
+		}
+		else
+		{
+			arr = (T *)aligned_alloc(alignment,sizeof(T)*size);
+
 		}
 
 	}
@@ -33,17 +55,31 @@ public:
 	{
 		if(pinned)
 		{
-			munlock(arr,size);
-		}
 
-		if(arr!=nullptr)
-			free(arr);
+			if(CL_SUCCESS!=clEnqueueUnmapMemObject(cq->getQueue(),mem,arr,0,nullptr,nullptr))
+			{
+				std::cout<<"error: clunmap"<<std::endl;
+			}
+
+			if(CL_SUCCESS!=clReleaseMemObject(mem))
+			{
+				std::cout<<"error: clunmap release"<<std::endl;
+			}
+
+		}
+		else
+		{
+			if(arr!=nullptr)
+				free(arr);
+		}
 	}
 private:
 	const size_t size;
 	bool pinned;
-	T * const arr;
-
+	T * arr;
+	cl_mem mem;
+	std::shared_ptr<ClContext> cntxt;
+	std::shared_ptr<ClCommandQueue> cq;
 };
 
 
--- Page.h
+++ Page.h
@@ -9,6 +9,8 @@
 #define PAGE_H_
 
 #include<memory>
+#include"ClCommandQueue.h"
+#include"ClContext.h"
 #include"AlignedCpuArray.h"
 
 template<typename T>
@@ -16,7 +18,7 @@ class Page
 {
 public:
 	Page(){arr=nullptr; edited=false;targetGpuPage=-1;}
-	Page(int sz){ arr=std::shared_ptr<AlignedCpuArray<T>>(new AlignedCpuArray<T>(sz,4096,true)); edited=false; targetGpuPage=-1;}
+	Page(int sz, std::shared_ptr<ClContext> ctx, std::shared_ptr<ClCommandQueue> q){ arr=std::shared_ptr<AlignedCpuArray<T>>(new AlignedCpuArray<T>(ctx,q,sz,4096,true)); edited=false; targetGpuPage=-1;}
 	T get(int i){ return arr->getArray()[i]; }
 	void edit(int i, T val){ arr->getArray()[i]=val; edited=true; }
 	bool isEdited(){ return edited; }
